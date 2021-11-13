c44c222866b2b630d0bbdf686e1cff3c9b1dea98
Georgy Solovei <g.solovei@telesphorus.by>
2021-11-13 19:49:59 +0300

    set up basic project structure

--- flicker/flicker/Modules/Photo List/View/PhotoListViewController.swift
+++ flicker/flicker/Modules/Photo List/View/PhotoListViewController.swift
@@ -7,14 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class PhotoListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
     }
-
-
 }
-
