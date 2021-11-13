cb89b79e9adb8db1ce5391a7b26fa643e0068195
Georgy Solovei <g.solovei@telesphorus.by>
2021-11-13 18:20:50 +0300

    create empty app

---
+++ flicker/flicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flicker
+//
+//  Created by Georgy Solovei on 13.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
